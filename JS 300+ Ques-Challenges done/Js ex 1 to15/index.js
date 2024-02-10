2047b40e3b6ecd2e66cfb3e095ec0c1a79908c46
Bhawna Suman <63039132+Bhawna1110@users.noreply.github.com>
2024-02-10 19:45:01 +0530

    Add files via upload

---
+++ JS 300+ Ques-Challenges done/Js ex 1 to15/index.js
@@ -0,0 +1,6 @@
+let cartQuantity = 0;
+
+let myName = 'Bhawna';
+console.log(myName);
+
+let calculation = '';
---
+++ JS 300+ Ques-Challenges done/Js ex 1 to15/index1.js
@@ -0,0 +1,17 @@
+/* alert('Good morning!')
+alert('Bhawna')
+*/
+console.log(2+2);
+console.log(100-20-50+200);
+console.log('$'+3*(10*1 + 3*8 + 1*5));
+console.log('$'+(185 + 2*75)/10);
+console.log('$'+(185 + 2*75)/100);
+console.log('$'+2*(185 + 2*75)/100);
+console.log('My name is Bhawna');
+console.log ('Total cost:$'+(599+295)/100);
+console.log (`Total cost:$${(599+295)/100}`);
+
+const isName='bhawna';
+console.log(`My name is:${isName}`);
+const cost='$'+(5+ 2*3 +9);
+console.log(`Cost of food:${cost}`);
